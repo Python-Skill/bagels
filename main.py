f56bb58f5fe77d2b014ffbc46c298a53b973f7d5
RipeCherries <alexeyegoshin0403@mail.ru>
2022-07-05 20:45:08 +0700

    Made the main game cycle

--- main.py
+++ main.py
@@ -56,6 +56,42 @@ def get_hints(secret_number: str, value: str):
 def main():
     show_info()
 
+    while True:
+        attempt_counter = 1
+
+        secret_number = get_secret_number()
+
+        while attempt_counter < 11:
+            print("\nПопытка #", attempt_counter)
+            attempt = input("> ")
+
+            if input_check(attempt):
+                if attempt == secret_number:
+                    print("Вы угадали!")
+                    break
+                else:
+                    print(*get_hints(secret_number, attempt))
+            else:
+                print("Введено неверное значение, попытка провалена!")
+
+            attempt_counter += 1
+
+        if attempt_counter > 10:
+            print("\nВы проиграли! Загаданное число -", secret_number)
+
+        print("\nХотите сыграть снова? (да или нет)")
+        key = input("> ")
+
+        if key.lower() in ["да", "нет"]:
+            if key.lower() == "да":
+                continue
+            else:
+                print("Спасибо за игру!")
+                break
+        else:
+            print("Введено неверное значение, завершение программы!")
+            break
+
 
 if __name__ == '__main__':
     main()
